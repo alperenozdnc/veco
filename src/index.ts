2513baedfb5a8897c1e3be9d80fb49fc7046f341
alperenozdnc <alperenozdincc@gmail.com>
2025-04-17 18:34:34 +0300

    feat(master): init command handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
+interface Command {
+    callers: string[];
+    action: Function;
+}
+
 function main(args: string[]) {
-    console.log(args);
+    const commands: Command[] = [
+        {
+            callers: ["help", "-h", "--help"],
+            action: () => console.log("help command")
+        }
+    ];
+
+    const helpCommand = commands[0];
+
+    // automatically run help command if no args are provided
+    if (args.length === 0) {
+        helpCommand.action();
+        return;
+    }
+
+    const userInput = args[0];
+
+    // handle command
+    for (let command of commands) {
+        if (command.callers.includes(userInput)) {
+            command.action();
+            return;
+        }
+    }
+
+    // handle command not found
+    console.log(`The command '${userInput}' does not exist.`);
+    console.log(`Type ${helpCommand.callers.map((caller: string) => `"${caller}"`).join(", ")} for help.`)
 }
 
 // file is not imported
