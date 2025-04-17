c4369f6132bae805f8b879597e4133cbd753c773
alperenozdnc <alperenozdincc@gmail.com>
2025-04-17 18:12:36 +0300

    chore(master): main function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-console.log("hello world");
+function main(args: string[]) {
+    console.log(args);
+}
 
+// file is not imported
+if (require.main === module) {
+    // first 2 args are node and the script being ran
+    main(process.argv.slice(2));
+}
