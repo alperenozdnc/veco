ed98d8dd5390c85072402eaee7c4bfdd00a73074
alperenozdnc <alperenozdincc@gmail.com>
2025-04-18 14:06:25 +0300

    feat(master): `LogWarning` util

---
+++ src/utils/LogWarning.ts
@@ -0,0 +1,3 @@
+export function LogWarning(msg: string) {
+    console.log(`\u001b[33mWARNING: ${msg}\u001b[0m`);
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { LogError } from "./LogError";
 export { CheckVecoDirectory } from "./CheckVecoDirectory";
 export { GetVecoDirectory } from "./GetVecoDirectory";
+export { LogWarning } from "./LogWarning";
