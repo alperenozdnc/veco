101715292f1adc06425002a3c8ffb575f80110b7
alperenozdnc <alperenozdincc@gmail.com>
2025-04-19 01:19:31 +0300

    chore(master): init some constants

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+import { GetVecoDirectory } from "../utils";
+
+export const VECO_DIR = GetVecoDirectory();
+export const IGNOREFILE_PATH = `${VECO_DIR}/.vecoig`;
+export const FOCUSFILE_PATH = `${VECO_DIR}/.veco/focus`
