2e181c37a5ff5a5dd18a28ae1c68497363649977
alperenozdnc <alperenozdincc@gmail.com>
2025-04-21 17:38:27 +0300

    chore(master): `REF_PATH` constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,4 @@ import { getVecoDir } from "../utils";
 export const VECO_DIR = getVecoDir();
 export const IGNOREFILE_PATH = `${VECO_DIR}/.vecoig`;
 export const FOCUSFILE_PATH = `${VECO_DIR}/.veco/focus`
+export const REF_PATH = `${VECO_DIR}/.veco/ref`
