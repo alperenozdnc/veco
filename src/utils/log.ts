ac4ca28ceffecdc65d97a418f06f592d1fd51be5
alperenozdnc <alperenozdincc@gmail.com>
2025-04-21 16:36:16 +0300

    fix(master): error and warn were reversed

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,4 +1,4 @@
 export const log = {
-    error: (msg: string) => console.log(`\u001b[33m[WARNING]: ${msg}\u001b[0m`),
-    warning: (msg: string) => console.log(`\x1b[31m[ERROR] ${msg}\x1b[0m`)
+    warning: (msg: string) => console.log(`\u001b[33m[WARNING]: ${msg}\u001b[0m`),
+    error: (msg: string) => console.log(`\x1b[31m[ERROR] ${msg}\x1b[0m`)
 }
