01751d5d4d15c05f5880d10fa7fb6da1effaba89
alperenozdnc <alperenozdincc@gmail.com>
2025-04-25 04:46:33 +0300

    feat(master): `view ignores` cmd

--- src/commands/view/view.ts
+++ src/commands/view/view.ts
@@ -1,3 +1,21 @@
+import fs from "fs";
+
+import { checkVecoDir, log } from "../../utils";
+import { IGNOREFILE_PATH } from "../../constants";
+
 export function view(args: string[]) {
-    console.log("view cmd");
+    if (!checkVecoDir()) return log.error("this command must be run in a veco directory");
+
+    if (args[0] === "ignores") {
+        if (args.length > 1) log.warning(`ignoring args after 'ignores'`);
+        if (!fs.existsSync(IGNOREFILE_PATH)) return log.error("no ignore file, create .vecoig file to see ignores");
+
+        const IGNORES = fs.readFileSync(IGNOREFILE_PATH).toString().split("\n");
+
+        IGNORES.pop();
+
+        console.log(`ignores (${IGNORES.length} total): `);
+
+        for (const ignore of IGNORES) console.log(`  ${ignore}`)
+    }
 }
