4f582beea7cf64481ae596af1d7499191319ec1c
alperenozdnc <alperenozdincc@gmail.com>
2025-04-25 20:37:08 +0300

    fix(master): arg order

--- src/commands/create/logUsage.ts
+++ src/commands/create/logUsage.ts
@@ -4,8 +4,8 @@ export function logUsage() {
     log.usage(
         "veco %cmd0 %cmd1 %opt0 %arg0 %opt1 %arg1",
         ["create", "change"],
+        ["message", "description"],
         [["-M", "msg", "--message"],
         ["-D", "desc", "--description"]],
-        ["message", "description"]
     );
 }
