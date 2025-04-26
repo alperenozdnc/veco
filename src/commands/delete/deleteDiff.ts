38614a0fda2316a77015b2147366f569d788fec0
alperenozdnc <alperenozdincc@gmail.com>
2025-04-26 22:57:13 +0300

    feat(master): `delete diff` command reverts back to change

--- src/commands/delete/deleteDiff.ts
+++ src/commands/delete/deleteDiff.ts
@@ -1,3 +1,33 @@
+import fs from "fs";
+
+import { REF_PATH, VECO_DIR } from "../../constants";
+import { log } from "../../utils";
+import { File } from "../../interfaces";
+
+import { compareTwoTrees } from "../create/compareTwoTrees";
+import { createFileTree } from "../create/createFileTree";
+import { updateTree } from "../create/updateTree";
+
 export function deleteDiff() {
-    console.log("Reverting back to last change...");
+    const ORDERFILE_PATH = `${VECO_DIR}/.veco/order`;
+
+    if (!fs.existsSync(ORDERFILE_PATH)) {
+        log.error("no changes to revert back to");
+        return;
+    }
+
+    const ORDERFILE = fs.readFileSync(ORDERFILE_PATH).toString().split("\n");
+
+    ORDERFILE.pop();
+
+    const ID = ORDERFILE[ORDERFILE.length - 1];
+
+    let REF_TREE = createFileTree(REF_PATH, true) as File[];
+    const CURR_TREE = createFileTree(`${VECO_DIR}`) as File[];
+
+    const differences = compareTwoTrees(CURR_TREE, REF_TREE)!;
+
+    updateTree(VECO_DIR, differences, false);
+
+    console.log(`Successfully reverted back to ${ID}`);
 }
