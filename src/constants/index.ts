d914d78af540e6aed63f6d6716b789db90d720dc
alperenozdnc <alperenozdincc@gmail.com>
2025-04-27 14:18:56 +0300

    feat(master): `OPERATION_NAMES` constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+import { Operation } from "../types";
 import { getVecoDir } from "../utils";
 
 export const VECO_DIR = getVecoDir();
@@ -5,3 +6,5 @@ export const IGNOREFILE_PATH = `${VECO_DIR}/.vecoig`;
 export const FOCUSFILE_PATH = `${VECO_DIR}/.veco/focus`
 export const REF_PATH = `${VECO_DIR}/.veco/ref`
 export const ORDERFILE_PATH = `${VECO_DIR}/.veco/order`;
+
+export const OPERATION_NAMES: Operation[] = ["MOD", "DEL", "INIT"];
