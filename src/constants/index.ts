1ff817b18f3d375d2ccba4ad7fc69f42fd8e3ffd
alperenozdnc <alperenozdincc@gmail.com>
2025-04-27 18:18:38 +0300

    chore(master): `LOCKFILE_PATH` constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,5 +6,6 @@ export const IGNOREFILE_PATH = `${VECO_DIR}/.vecoig`;
 export const FOCUSFILE_PATH = `${VECO_DIR}/.veco/focus`
 export const REF_PATH = `${VECO_DIR}/.veco/ref`
 export const ORDERFILE_PATH = `${VECO_DIR}/.veco/order`;
+export const LOCKFILE_PATH = `${VECO_DIR}/.veco/LOCK`;
 
 export const OPERATION_NAMES: Operation[] = ["MOD", "DEL", "INIT"];
