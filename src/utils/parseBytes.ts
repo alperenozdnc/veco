8ca8971c661ccdfa668c6c5ffc7363ad421feb72
alperenozdnc <alperenozdincc@gmail.com>
2025-04-28 14:00:19 +0300

    chore(master): more readable code

--- src/utils/parseBytes.ts
+++ src/utils/parseBytes.ts
@@ -1,15 +1,10 @@
 export function parseBytes(bytes: number) {
-    const sizes = ["B", "KB", "MB", "GB", "TB"]
+    if (bytes == 0) return "n/a";
 
-    if (bytes == 0) {
-        return "n/a"
-    }
-
+    const sizes = ["B", "KB", "MB", "GB", "TB"];
     const i = Math.floor(Math.log(bytes) / Math.log(1024));
 
-    if (i == 0) {
-        return bytes + " " + sizes[i]
-    }
+    if (i == 0) return bytes + " " + sizes[i];
 
-    return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i]
+    return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i];
 }
