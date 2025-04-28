1ae19edf7ba48eb18a6c9e656033e9b77c6d7983
alperenozdnc <alperenozdincc@gmail.com>
2025-04-28 13:57:37 +0300

    refactor(master): object deconstruct instead of spamming dots

--- src/utils/printDiff.ts
+++ src/utils/printDiff.ts
@@ -4,19 +4,21 @@ import { Difference } from "../interfaces";
 import { padLeft, parseBytes } from ".";
 
 export function printDiff(diff: Difference) {
+    const { file, newFile, operation } = diff;
+
     let isMODoperation = false;
     let deltaChar = 0;
     const output = [];
 
-    if (diff.operation === "MOD") {
-        deltaChar = diff.newFile!.content.length - diff.file.content.length;
+    if (operation === "MOD" && newFile) {
+        deltaChar = newFile.content.length - file.content.length;
 
         isMODoperation = true;
-        output.push(`MOD -> '${diff.file.name}' (${deltaChar > 0 ? "+" : ""}${deltaChar} chars)`)
+        output.push(`MOD -> '${file.name}' (${deltaChar > 0 ? "+" : ""}${deltaChar} chars)`)
     } else if (diff.operation === "INIT") {
-        output.push(`${diff.operation} -> '${diff.file.name}' (${parseBytes(fs.statSync(diff.file.path).size)})`)
+        output.push(`${operation} -> '${file.name}' (${parseBytes(fs.statSync(file.path).size)})`)
     } else {
-        output.push(`${diff.operation} -> '${diff.file.name}'`)
+        output.push(`${operation} -> '${file.name}'`)
     }
 
     padLeft(output);
